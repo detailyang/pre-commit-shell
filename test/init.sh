95dfb625ac1d2bd16c11fa0f9a8a8591fbbd2b64
detailyang <detailyang@gmail.com>
2016-05-28 20:18:02 +0800

    real testcase to cover test

--- test/init.sh
+++ test/init.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
 cat << EOS > .pre-commit-config.yaml
--   repo: https://github.com/detailyang/pre-commit-shell
+-   repo: $(pwd)
     sha: $(git rev-parse HEAD)
     hooks:
     -   id: shell-lint
 EOS
 
 tmpdir=$(mktemp -t pre-commit-shell  -d)
-cp -r . $tmpdir
+cp -r test/* "$tmpdir"
+cp -r test/.* "$tmpdir"
+pushd "$tmpdir"
 git init
+git add .pre-commit-config.yaml; git commit -a -m "init test case"
 pre-commit
-rm -rf $tmpdir
+git add test.sh --all
+git commit -a -m "test"
+popd
