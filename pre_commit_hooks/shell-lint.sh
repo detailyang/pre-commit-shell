062f0b028ae65827e04f91c1e6738cfcbe9b337f
detailyang <detailyang@gmail.com>
2019-05-22 16:05:49 +0800

    feat: use command to avoid coverage

--- pre_commit_hooks/shell-lint.sh
+++ pre_commit_hooks/shell-lint.sh
@@ -7,9 +7,7 @@ set -o nounset
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
-echo 'Begin shellcheck'
-
-if ! which shellcheck &>/dev/null; then
+if ! command which shellcheck &>/dev/null; then
   >&2 echo 'shellcheck command not found'
   exit 1
 fi
